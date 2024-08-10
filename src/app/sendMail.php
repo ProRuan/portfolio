4132229324191f175e938839834734947799520e
Your Name <you@example.com>
2024-08-10 15:34:50 +0200

    exchanged hero email

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'ahasver@loewenkoenig@gmail.com';  
+            $recipient = 'rudolf.sachslehner@gmx.at';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
