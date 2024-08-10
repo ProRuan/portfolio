197dcf5165a8de7933ea6c86cbb62784b5363d8d
Your Name <you@example.com>
2024-08-10 14:48:06 +0200

    styled language button

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LinkComponent } from '../link/link.component';
 import { Link } from '../../interfaces/link';
 import { LinkService } from '../../services/link.service';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-menu',
@@ -12,9 +13,28 @@ import { LinkService } from '../../services/link.service';
   styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
+  linkData: LinkService = inject(LinkService);
+  langData: LanguageService = inject(LanguageService);
   links: Link[];
+  lang: string = 'EN';
 
-  constructor(private linkData: LinkService) {
+  constructor() {
+    this.links = this.linkData.get(4);
+  }
+
+  changeLang() {
+    this.updateLang();
+    this.updateLinks();
+  }
+
+  updateLang() {
+    this.langData.change();
+    let currLang = this.langData.currLang;
+    this.lang = currLang == 'german' ? 'DE' : 'EN';
+  }
+
+  updateLinks() {
+    this.linkData.set();
     this.links = this.linkData.get(4);
   }
 
