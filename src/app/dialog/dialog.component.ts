e471c6f82578b47682f797c99ad3655e6600be40
Your Name <you@example.com>
2024-08-22 22:00:25 +0200

    set transition of privacy policy

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component';
+import { LinkService } from '../shared/services/link.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [PrivacyPolicyComponent],
+  imports: [PrivacyPolicyComponent, CommonModule],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss',
 })
@@ -13,6 +15,21 @@ import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.compone
  * Represents a dialog component.
  */
 export class DialogComponent {
+  /**
+   * Creates a dialog component.
+   * @param linkData - The linkData to apply.
+   */
+  constructor(private linkData: LinkService) {}
+
+  /**
+   * Shows the privacy policy.
+   * @returns - The class to apply.
+   */
+  show() {
+    let opened = this.linkData.dialogOpened;
+    return opened ? 'visible' : '';
+  }
+
   /**
    * Stops an event.
    * @param event - The event to stop.
